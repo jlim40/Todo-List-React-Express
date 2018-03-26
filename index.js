d3aacf2dac00c26b6a7e7f5a88f59d2524a7c6af
Jason Lim <jihoon2514@gmail.com>
2018-03-25 23:02:09 -0400

    fixed index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/api/message', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
-  // like our main.js file, or main.css file!
+  // like our main.js, or main.css file!
   app.use(express.static('client/build'));
 
   // Express will serve up the index.html file
@@ -20,4 +20,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 const PORT = process.env.PORT || 5000;
+console.log(PORT);
 app.listen(PORT);
