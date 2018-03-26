c63e9f1a0a368953c8d9956c9560d10d57afa9b1
Jason Lim <jihoon2514@gmail.com>
2018-03-25 23:44:30 -0400

    Deployment Try2

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ app.get('/api/message', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js, or main.css file!
-  app.use(express.static('../client/build'));
+  app.use(express.static('../../client/build'));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
+    console.log('dirname??', __dirname);
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
